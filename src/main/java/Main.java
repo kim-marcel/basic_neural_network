c4c23476f7574941ca47a4279269788795e7c5fc
Kim Feichtinger <KimFeichtinger@Kims-MacBook-Pro.local>
2018-03-05 00:35:31 +0000

    Added NeuralNetwork and Main class for testing purpose

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,84 @@
-package PACKAGE_NAME;
+import org.ejml.simple.SimpleMatrix;
+
+import java.util.Random;
 
 /**
  * Created by KimFeichtinger on 04.03.18.
  */
 public class Main {
+
+    public static void main(String args[]){
+
+        SimpleMatrix[] trainingData = new SimpleMatrix[4];
+        for (int i = 0; i < trainingData.length; i++) {
+            trainingData[i] = new SimpleMatrix(2,1);
+        }
+
+        trainingData[0].set(0,0,0);
+        trainingData[0].set(1,0,0);
+
+        trainingData[1].set(0,0,1);
+        trainingData[1].set(1,0,1);
+
+        trainingData[2].set(0,0,1);
+        trainingData[2].set(1,0,0);
+
+        trainingData[3].set(0,0,0);
+        trainingData[3].set(1,0,1);
+
+        SimpleMatrix[] trainingDataTargets = new SimpleMatrix[4];
+        for (int i = 0; i < trainingDataTargets.length; i++) {
+            trainingDataTargets[i] = new SimpleMatrix(1,1);
+        }
+
+        trainingDataTargets[0].set(0,0,0);
+
+        trainingDataTargets[1].set(0,0,0);
+
+        trainingDataTargets[2].set(0,0,1);
+
+        trainingDataTargets[3].set(0,0,1);
+
+        SimpleMatrix testData1 = new SimpleMatrix(2,1);
+        testData1.set(0,0,0);
+        testData1.set(1,0,0);
+
+        SimpleMatrix testData2 = new SimpleMatrix(2,1);
+        testData2.set(0,0,1);
+        testData2.set(1,0,1);
+
+        SimpleMatrix testData3 = new SimpleMatrix(2,1);
+        testData3.set(0,0,0);
+        testData3.set(1,0,1);
+
+        SimpleMatrix testData4 = new SimpleMatrix(2,1);
+        testData4.set(0,0,1);
+        testData4.set(1,0,0);
+
+//        // IMPORTANT: inputs has to be a one column matrix!
+//        SimpleMatrix inputs = new SimpleMatrix(2,1);
+//        inputs.set(0.24);
+//
+//        // correct answers for the given input
+//        SimpleMatrix targets = new SimpleMatrix(1,1);
+//        targets.set(0.1);
+
+        NeuralNetwork nn = new NeuralNetwork(2,2, 1);
+
+        //System.out.println(nn.feedForward(inputs));
+
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < trainingData.length; j++) {
+                nn.train(trainingData[j], trainingDataTargets[j]);
+            }
+        }
+
+        System.out.println("0,0: " + nn.feedForward(testData1));
+        System.out.println("1,1: " + nn.feedForward(testData2));
+        System.out.println("0,1: " + nn.feedForward(testData3));
+        System.out.println("1,0: " + nn.feedForward(testData4));
+//        nn.train(inputs, targets);
+    }
+
+
 }
--- src/main/java/NeuralNetwork.java
+++ src/main/java/NeuralNetwork.java
@@ -1,7 +1,121 @@
-package PACKAGE_NAME;
+import org.ejml.simple.SimpleMatrix;
+
+import java.util.Random;
 
 /**
  * Created by KimFeichtinger on 04.03.18.
  */
 public class NeuralNetwork {
+
+    private static final double LEARNING_RATE = 0.1;
+
+    Random r = new Random();
+
+    // variables which store the "size" of the neural network
+    private int inputNodes;
+    private int hiddenNodes;
+    private int outputNodes;
+
+    private SimpleMatrix weightsIH; // matrix with weights between input and hidden layer
+    private SimpleMatrix weightsHO; // matrix with weights between hidden and output layer
+
+    private SimpleMatrix biasH;
+    private SimpleMatrix biasO;
+
+    // Constructor
+    // generate a new neural network with 1 hidden layer with the given amount of nodes in the layers
+    public NeuralNetwork(int inputNodes, int hiddenNodes, int outputNodes){
+        this.inputNodes = inputNodes;
+        this.hiddenNodes = hiddenNodes;
+        this.outputNodes = outputNodes;
+
+        // initialize the weights between the layers and fill them with random values
+        weightsIH = SimpleMatrix.random64(hiddenNodes, inputNodes, -1, 1, r);
+        weightsHO = SimpleMatrix.random64(outputNodes, hiddenNodes, -1, 1, r);
+
+        // initialize the biases and fill them with random values
+        biasH = SimpleMatrix.random64(hiddenNodes, 1, -1, 1, r);
+        biasO = SimpleMatrix.random64(outputNodes, 1, -1, 1, r);
+    }
+
+    // feedForward method, input is a one column matrix with the input values
+    public SimpleMatrix feedForward(SimpleMatrix inputs){
+        SimpleMatrix hidden = calculateLayer(weightsIH, biasH, inputs);
+        SimpleMatrix output = calculateLayer(weightsHO, biasO, hidden);
+        return output;
+    }
+
+    public void train(SimpleMatrix inputs, SimpleMatrix targets){
+        // calculate outputs of hidden and output layer for the given inputs
+        SimpleMatrix hidden = calculateLayer(weightsIH, biasH, inputs);
+        SimpleMatrix outputs = calculateLayer(weightsHO, biasO, hidden);
+
+        // calculate the errors
+        SimpleMatrix outputErrors = targets.minus(outputs); // errors that happen in the output layer
+        SimpleMatrix hiddenErrors = weightsHO.transpose().mult(outputErrors); // errors that happen in the hidden layer
+
+        // calculate gradients and delta weights
+        SimpleMatrix outputGradient = calculateGradient(outputs, outputErrors);
+        SimpleMatrix outputDeltaWeights = calculateDeltas(outputGradient, hidden);
+
+        SimpleMatrix hiddenGradient = calculateGradient(hidden, hiddenErrors);
+        SimpleMatrix hiddenDeltaWeights = calculateDeltas(hiddenGradient, inputs);
+
+        // adjust the weights by deltas (add deltas to the current weights)
+        weightsHO = weightsHO.plus(outputDeltaWeights);
+        weightsIH = weightsIH.plus(hiddenDeltaWeights);
+
+        // adjust the bias by its deltas (gradients)
+        biasO = biasO.plus(outputGradient);
+        biasH = biasH.plus(hiddenGradient);
+    }
+
+    // ***** Helping methods: *****
+
+    // generic function to calculate one layer
+    private SimpleMatrix calculateLayer(SimpleMatrix weights, SimpleMatrix bias, SimpleMatrix input){
+        // calculate outputs of layer
+        SimpleMatrix result = weights.mult(input);
+        // add bias to outputs
+        result = result.plus(bias);
+        // apply activation function and return result
+        result = applySigmoid(result, false);
+        return result;
+    }
+
+    private SimpleMatrix calculateGradient(SimpleMatrix layer, SimpleMatrix error){
+        SimpleMatrix gradient = applySigmoid(layer, true);
+        gradient = gradient.elementMult(error);
+        return gradient.scale(LEARNING_RATE);
+    }
+
+    private SimpleMatrix calculateDeltas(SimpleMatrix gradient, SimpleMatrix layer){
+        return gradient.mult(layer.transpose());
+    }
+
+    private SimpleMatrix applySigmoid(SimpleMatrix input, boolean derivative){
+        SimpleMatrix output = new SimpleMatrix(input.numRows(), input.numCols());
+        for (int i = 0; i < input.numRows(); i++) {
+            for (int j = 0; j < input.numCols(); j++) {
+                double value = input.get(i, j);
+                // apply dsigmoid if derivative = true, otherwise usual sigmoid
+                if(derivative){
+                    output.set(i, j, dsigmoid(value));
+                }else {
+                    output.set(i, j, sigmoid(value));
+                }
+            }
+        }
+
+        return output;
+    }
+
+    private double sigmoid(double input){
+        return 1 / (1 + Math.exp(-input));
+    }
+
+    // derivative of sigmoid (not real derivative because sigmoid function has already been applied to the input)
+    private double dsigmoid(double input){
+        return input * (1 - input);
+    }
 }
