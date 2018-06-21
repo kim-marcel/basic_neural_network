3b34acc84a98b9ea128cf5221ad1f4002d1fe425
kim-marcel <kim.feichtinger@gmx.de>
2018-06-21 22:12:56 +0200

    Prepare project for JUnit-Tests
    - add JUnit 5 dependency in pom.xml
    - add maven platform surefire plugin in pom.xml (enables to run tests with maven)
    - add a first Test (only example, not a real test)

---
+++ src/test/java/basicneuralnetwork/utilities/MatrixUtilitiesTest.java
@@ -0,0 +1,12 @@
+package basicneuralnetwork.utilities;
+
+import org.junit.jupiter.api.Test;
+
+class MatrixUtilitiesTest {
+
+    @Test
+    public void myFirstTest(){
+        System.out.println("Test is workings");
+    }
+
+}
